5195e87bd5c932f107e73a7e7bd41320952629ed
sbushmanov <sergey.bushmanov@gmail.com>
2014-07-21 20:44:05 +0400

    R Programming: Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+# 
+# Below is R code for a pair of functions that
+#
+# (1) makeCacheMatrix: creates a special "matrix" object that can cache its inverse.
+# (2) cacheSolve: computes inverse of the matrix unless it already exists in cache
+# In this case inverse is retrieved from the cache. 
+#
+# Inverse is computed via # "solve" function, assuming inverse of a matrix exists.
+#
+# The pair is meant to make use of cache to reduce CPU load and increase workload
+# of coder
+#
+################################################################################
 
-## Write a short comment describing this function
+# makeCacheMatrix function creates a list of 4 functions to:
+#       - set a matrix: makeCacheMatrix$set()
+#       - get a matrix: makeCacheMatrix$get()
+#       - set inverse of a matrix: makeCacheMatrix$setinverse()
+#       - get inverse of a matrix: makeCacheMatrix$getinverse()
+# See examples in the Examples Section
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve function checks if inverse exists and:
+#       - if not -- calculates and stores it
+#       - if yes -- retrives from cache while printing message
+# See examples in the Examples section
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+# Examples: 
+
+set.seed(1)                     # set seed to ensure reproducibility
+mat <- matrix(rnorm(9), 3)      # generate random matrix
+tr <- makeCacheMatrix(mat)      # make an object
+tr$get()                        # see it
+mat                             # sanity check
+tr$getinverse()                 # no inverse so far
+cacheSolve(tr)                  # first attempt, inverse is calculted
+cacheSolve(tr)                  # second attempt, invers retrieved from cache
+identical(cacheSolve(tr), solve(mat))          # another sanity check
